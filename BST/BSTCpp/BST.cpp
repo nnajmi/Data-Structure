fe4b66a7704df524660f740bac9600f22bc3e678
nnajmi <najmeh.najmi@gmail.com>
2020-09-20 20:59:04 -0700

    1

--- BST/BSTCpp/BST.cpp
+++ BST/BSTCpp/BST.cpp
@@ -23,6 +23,9 @@ class BST
         int Height(Node *p);
         Node * InPre(Node *p);
         Node * InSucc(Node *p); 
+        Node * Delete(Node *p, int key);
+        //void CreateFromPreorder(int pre[], int n); //not working
+        void CreateFromPreorder(int pre[], int n);
 };
 void BST::Inorder(Node *p)
 {
@@ -98,6 +101,7 @@ Node * BST::RInsert(Node *p, int key)
     {
         t = new Node;
         t->data = key;
+ 
         t->lchild = t->rchild = nullptr;
         if(this->Getroot() == nullptr)
             root = t;
@@ -120,20 +124,144 @@ int BST::Height(Node *p)
 }
 Node * BST::InPre(Node *p)
 {
+    p = p->lchild;
     while(p && p->rchild)
         p = p->rchild;
     return p;
 }
 Node * BST::InSucc(Node *p)
 {
+    p = p->rchild;
     while(p && p->lchild)
         p = p->lchild;
     return p;
+}
+Node * BST::Delete(Node *p, int key)
+{
+    Node *q;
+    if(p == nullptr)
+        return nullptr;
+    if(p->lchild == nullptr && p->rchild == nullptr)
+    {
+        if(p == root)
+            root = nullptr;
+        delete p;
+        return nullptr;
+    }
+    if( key < p->data)
+        p->lchild = Delete(p->lchild, key);
+    else if(key > p->data)
+        p->rchild = Delete(p->rchild, key);
+    else
+    {
+        if(Height(p->lchild) > Height(p->rchild))
+        {
+            q = InPre(p);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }  
+    }
+    return p;
+}
+// void BST::CreateFromPreorder(int pre[], int n)
+// {
+//     int comp;
+//     //Create root node
+//     int i = 0;
+//     root = new Node;
+//     root->data = pre[i++];
+//     root->lchild = root->rchild = nullptr;
+
+//     //Iterative steps
+//     Node *t;
+//     Node *p = root;
+//     stack <Node *> stk;
+//     while(i < n)
+//     {
+//         //Left child case
+//         if(pre[i] < p->data)
+//         {
+//             t = new Node;
+//             t->data = pre[i++];
+//             t->lchild = t->rchild = nullptr;
+//             p->lchild = t;
+//             stk.push(p);
+//             p = t;
+//         }
+//         //Right child case
+//         else
+//         {
+//             if(stk.empty())
+//                 comp = 32767;
+//             else
+//                 comp = stk.top()->data;
+//             if(pre[i] > p->data && pre[i] < comp)
+//             {
+//                 t = new Node;
+//                 t->data = pre[i++];
+//                 t->lchild = t->rchild = nullptr;
+//                 p->rchild = t;
+//                 p = t;
+//             }
+//             else
+//             {
+//                 p = stk.top();
+//                 stk.pop();
+//             }            
+//         }
+//     }
+// }
+void BST::CreateFromPreorder(int pre[], int n)
+{
+    int comp;
+    //Create root node
+    int i = 0;
+    root = new Node;
+    root->data = pre[i++];
+    root->lchild = root->rchild = nullptr;
+
+    //Iterative steps
+    Node *t;
+    Node *p;
+    stack <Node *> stk;
+    stk.push(root);
+    while(i < n)
+    {
+        p = nullptr;
+        while(!stk.empty() && pre[i] > stk.top()->data)
+        {
+            p = stk.top();
+            stk.pop();
+        }
+        if(p != nullptr)
+        {
+            t = new Node;
+            t->data = pre[i++];
+            t->lchild = t->rchild = nullptr;
+            p->lchild = t;
+            stk.push(p->rchild);
+        }   
+        else
+        {
+            t = new Node;
+            t->data = pre[i++];
+            t->lchild = t->rchild = nullptr;
+            stk.top()->rchild = t;
+            stk.push(stk.top()->rchild);
+        }
+    }
 }
 int main()
 {
     BST bst;
     Node *temp;
+    
     //Iterative Insert
     bst.IInsert(10);
     bst.IInsert(5);
@@ -142,28 +270,47 @@ int main()
     bst.IInsert(30);
     bst.Inorder(bst.Getroot());
     cout << endl;
+    
     //Recursive Insert
     bst.RInsert(bst.Getroot(), 50);
     bst.RInsert(bst.Getroot(), 70);
     bst.RInsert(bst.Getroot(), 1);
     bst.Inorder(bst.Getroot());
     cout << endl;
+    
     //Iterative Search
     temp = bst.ISearch(2);
     if(temp != nullptr)
         cout << temp->data << " was found" << endl;
     else
         cout << "Key was not found." <<endl;
+    
     //Recursive Search
     temp = bst.RSearch(bst.Getroot(), 2);
     if(temp != nullptr)
         cout << temp->data << " was found" << endl;
     else
         cout << "Key was not found." <<endl;
+    
     //Height
     cout << "Height " << bst.Height(bst.Getroot()) << endl;
+    
     //Inorder predecessor and inorder successor
-    cout << "Inorder predecessor " << bst.InPre(bst.Getroot()->lchild)->data << endl;
-    cout << "Inorder successor " << bst.InSucc(bst.Getroot()->rchild)->data << endl;
+    cout << "Inorder predecessor " << bst.InPre(bst.Getroot())->data << endl;
+    cout << "Inorder successor " << bst.InSucc(bst.Getroot())->data << endl;
+
+    // Delete
+    bst.Delete(bst.Getroot(), 5);
+    bst.Inorder(bst.Getroot());
+    cout << endl;
+
+    //Create BST from Preorder
+    //int pre[] = {30, 20, 10, 15, 25, 40, 50, 45};
+    int pre[] = {10, 5, 20, 8, 30, 50, 70, 1};
+    BST b;
+    int n = sizeof(pre) / sizeof(pre[0]);
+    b.CreateFromPreorder(pre, n);
+    b.Inorder(b.Getroot());
+    //cout << "root " << b.Getroot()->lchild->lchild->data;
     return 0;
 }
