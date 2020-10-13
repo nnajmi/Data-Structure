d2cb5d8994cd64cb57a4797bfc2bcf8fe81ec10d
nnajmi <najmeh.najmi@gmail.com>
2020-10-13 00:16:19 -0700

    *

--- Sorts/Sorting.C
+++ Sorts/Sorting.C
@@ -44,13 +44,60 @@ void InsertionSort(int A[], int n)
         A[j+1] = x;
     }
 }
+//Selection sort is not stable nor adaptive.
+void SelectionSort(int A[], int n)
+{
+    int i, j, k;
+    for(i = 0; i < n-1; i++)
+    {
+        for(j = k = i; j < n; j++)
+        {
+            if(A[j] < A[k])
+                k = j;
+        }
+        Swap(&A[i], &A[k]);
+    }
+}
+//Quick Sort 
+int Partition(int A[], int l, int h)
+{
+    int pivot = A[l];
+    int i = l, j = h;
+
+    do
+    {
+        do{i++;} while(A[i] <= pivot);
+        do{j--;} while(A[j] > pivot);
+
+        if(i < j)
+            Swap(&A[i], &A[j]);
+    } while (i < j);
+    Swap(&A[l], &A[j]);
+    return j;
+}
+void QuickSort(int A[], int l, int h)
+{
+    int j;
+    if(l < h)
+    {
+        j = Partition(A, l, h);
+        QuickSort(A, l ,j);
+        QuickSort(A, j + 1, h);
+    }
+}
 int main()
 {
-    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
-    int n = 10;
+    // int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
+    // int n = 10;
 
     //BubbleSort(A, 10);
-    InsertionSort(A, 10);
+    //InsertionSort(A, 10);
+    // SelectionSort(A, 10);
+    
+    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2, __INT32_MAX__};
+    int n = 11;
+
+    QuickSort(A, 0, 10);
 
     for(int i = 0; i < n; i++)
         printf("%d ", A[i]);
